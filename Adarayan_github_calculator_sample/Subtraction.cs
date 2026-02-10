72e2c0773cfd51c2333a7ebbb0b27befa33b80eb
dingus <jose.302269@balagtas.sti.edu.ph>
2026-02-10 19:58:38 +0800

    feat: I added the Subtraction.cs, it gives the system a way to subtract the number that was given to the system.
    With the public int and return.

---
+++ Adarayan_github_calculator_sample/Subtraction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adarayan_github_calculator_sample
+{
+    internal class Subtraction
+    {
+        public int subtraction(int x, int y) //this gives the system the value  
+        {
+            return x - y; //this subtracts the number from the value
+        }
+    }
+}
